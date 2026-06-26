4aa57af8ddd4c732ae32df1ca2ceddeb25b308e0
Kaifffff <113055743+Kaifffff@users.noreply.github.com>
2026-06-26 11:34:29 +0000

    complete step 3

--- octofit-tracker/backend/src/database.ts
+++ octofit-tracker/backend/src/database.ts
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/octofit_db";
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/octofit_db";
 
 export const database = mongoose.connection;
 
-export const connectToDatabase = async () => {
-  if (database.readyState >= 1) {
+export async function connectToDatabase() {
+  if (database.readyState === 1) {
     return database;
   }
 
   await mongoose.connect(MONGO_URI);
+
   return database;
-};
+}
+
+export default connectToDatabase;
